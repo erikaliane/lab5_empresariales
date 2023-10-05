3cc3b80d65b8fa193522c32eff26a369fd11b945
erikaliane <105476284+erikaliane@users.noreply.github.com>
2023-10-05 18:26:26 -0500

    Add files via upload

---
+++ DesAppEmpreAvanzado-Lab05/Lab05/Listar.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab05
+{
+    /// <summary>
+    /// Lógica de interacción para Listar.xaml
+    /// </summary>
+    public partial class Listar : Window
+    {
+        public Listar()
+        {
+            InitializeComponent();
+
+            // Configurar la cadena de conexión a tu base de datos
+            string connectionString = "Data Source=DESKTOP-DE20VUT\\SQLEXPRESS ;Initial Catalog=Neptuno3;User ID=tecsup;Password=tecsup";
+
+            try
+            {
+                // Crear una conexión a la base de datos
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Crear un comando para ejecutar el procedimiento almacenado
+                    using (SqlCommand command = new SqlCommand("ListarProductos", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Crear un lector de datos
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            List<Producto> productos = new List<Producto>();
+
+                            // Leer los datos y agregarlos a la lista de productos
+                            while (reader.Read())
+                            {
+                                Producto producto = new Producto
+                                {
+                                    IdProducto = Convert.ToInt32(reader["idproducto"]),
+                                    NombreProducto = reader["nombreProducto"].ToString(),
+                                    IdProveedor = Convert.ToInt32(reader["idProveedor"]),
+                                    IdCategoria = Convert.ToInt32(reader["idCategoria"]),
+                                    CantidadPorUnidad = reader["cantidadPorUnidad"].ToString(),
+                                    PrecioUnidad = Convert.ToDecimal(reader["precioUnidad"]),
+                                    UnidadesEnExistencia = Convert.ToInt32(reader["unidadesEnExistencia"]),
+                                    UnidadesEnPedido = Convert.ToInt32(reader["unidadesEnPedido"]),
+                                    NivelNuevoPedido = Convert.ToInt32(reader["nivelNuevoPedido"]),
+                                    CategoriaProducto = reader["categoriaProducto"].ToString()
+                                };
+
+                                productos.Add(producto);
+                            }
+
+                            // Asignar la lista de productos al DataGrid
+                            dataGrid.ItemsSource = productos;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al consultar la base de datos: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ DesAppEmpreAvanzado-Lab05/Lab05/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            }
+            private void GuardarClick(object sender, RoutedEventArgs e)
+            {
+                try
+                {
+                    string connectionString = "Data Source=DESKTOP-DE20VUT\\SQLEXPRESS ;Initial Catalog=Neptuno3;User ID=tecsup;Password=tecsup";
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand cmd = new SqlCommand("CrearProducto", connection);
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@idProducto", int.Parse(txtIdProducto.Text));
+                        cmd.Parameters.AddWithValue("@nombreProducto", txtNombreProducto.Text);
+                        cmd.Parameters.AddWithValue("@idProveedor", int.Parse(txtIdProveedor.Text));
+                        cmd.Parameters.AddWithValue("@idCategoria", int.Parse(txtIdCategoria.Text));
+                        cmd.Parameters.AddWithValue("@cantidadPorUnidad", txtCantidadPorUnidad.Text);
+                        cmd.Parameters.AddWithValue("@precioUnidad", decimal.Parse(txtPrecioUnidad.Text));
+
+                        cmd.Parameters.AddWithValue("@unidadesEnExistencia", int.Parse(txtUnidadesEnExistencia.Text));
+                        cmd.Parameters.AddWithValue("@unidadesEnPedido", int.Parse(txtUnidadesEnPedido.Text));
+                        cmd.Parameters.AddWithValue("@nivelNuevoPedido", int.Parse(txtNivelNuevoPedido.Text));
+                        cmd.Parameters.AddWithValue("@categoriaProducto", txtCategoriaProducto.Text);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Producto ingresada correctamente.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al ingresar Producto: " + ex.Message);
+                }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Listar window = new Listar();
+            this.Close();
+            window.Show();
+        }
+    }
+}
